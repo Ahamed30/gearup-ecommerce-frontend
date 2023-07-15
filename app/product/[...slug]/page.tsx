4b7d493e13c26077b4bd04659e19bbe80d748870
Ahamed30 <dhanishahmed30@gmail.com>
2023-07-15 17:00:48 +0530

    initial commit pdp-page

---
+++ app/product/[...slug]/page.tsx
@@ -0,0 +1,10 @@
+interface PageProps {
+  params?: { slug: string[] };
+}
+export default async function ProductPage({ params }: PageProps) {
+  const slug = params?.slug || [];
+
+  const [id] = slug;
+
+  console.log("id", id);
+}
