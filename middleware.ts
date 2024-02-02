e2c288bac6be01c0303af957a1719222924a7025
Ahamed30 <dhanishahmed30@gmail.com>
2024-02-02 08:01:00 +0530

    resolve build failure

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
-import { getSession } from "next-auth/react";
+// import { getSession } from "next-auth/react";
 
 export async function middleware(request: NextRequest) {
   const response = NextResponse.next();
   const { nextUrl } = request;
 
-  const requestForNextAuth = {
-    headers: {
-      cookie: request.headers.get("cookie") ?? undefined,
-    },
-  };
+  // const requestForNextAuth = {
+  //   headers: {
+  //     cookie: request.headers.get("cookie") ?? undefined,
+  //   },
+  // };
 
-  const session = await getSession({ req: requestForNextAuth });
+  // const session = await getSession({ req: requestForNextAuth });
 
-  if (nextUrl.pathname === "/checkout" && !session) {
+  // Need to check session condition below (&& !session)
+  if (nextUrl.pathname === "/checkout") {
     const origPathName = nextUrl.pathname;
 
     nextUrl.pathname = "/auth/login";
@@ -23,7 +24,8 @@ export async function middleware(request: NextRequest) {
     return NextResponse.redirect(nextUrl);
   }
 
-  if (nextUrl.pathname.startsWith("/auth") && session) {
+  // Need to check session condition below (&& session)
+  if (nextUrl.pathname.startsWith("/auth")) {
     nextUrl.pathname = "/";
     return NextResponse.redirect(nextUrl);
   }
